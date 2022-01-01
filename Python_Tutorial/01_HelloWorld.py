e42d6bbc5370baddb1e3b2a0734f6c4e9fc24f4e
Ayush Kumar <62169991+ayush098-hub@users.noreply.github.com>
2022-01-01 15:11:38 +0530

    Create 01_HelloWorld.py

---
+++ Python_Tutorial/01_HelloWorld.py
@@ -0,0 +1,12 @@
+#This is the first tutorial of this series in which we'll basically learn about how we can print someting on the screen.
+
+# So we use print() function to print someting on screen.
+
+# Ex1- To print string, we print it in this way:
+print("Hello World)
+      
+# Ex2 - To print number, just print it without quotation marks
+      
+print(123)
+      
+#This is all about print function.
