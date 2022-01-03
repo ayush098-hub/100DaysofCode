9934720abaa64cd93a4bf7e31595da4ec4d3a7d8
Ayush Kumar <62169991+ayush098-hub@users.noreply.github.com>
2022-01-03 18:55:03 +0530

    Create D2_SumofArray.py

---
+++ Day2/D2_SumofArray.py
@@ -0,0 +1,13 @@
+Question_Link: https://practice.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1/?category[]=Arrays&category[]=Arrays&problemStatus=solved&page=1&query=category[]ArraysproblemStatussolvedpage1category[]Arrays
+def sum(arr):
+    sum=0
+
+    for i in range(len(arr)):
+        sum=sum+arr[i]
+    return sum
+
+
+
+arr=[1,2,3,4]
+
+print(sum(arr))
