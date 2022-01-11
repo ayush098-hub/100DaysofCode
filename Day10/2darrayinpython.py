2a69f9e0f64ffe84158198aa414851d091b6d7fe
Ayush Kumar <62169991+ayush098-hub@users.noreply.github.com>
2022-01-11 16:16:12 +0530

    Create 2darrayinpython.py

---
+++ Day10/2darrayinpython.py
@@ -0,0 +1,17 @@
+
+
+Student_dt = [ [72, 85, 87, 90, 69], [80, 87, 65, 89, 85], [96, 91, 70, 78, 97], [90, 93, 91, 90, 94], [57, 89, 82, 69, 60] ] 
+
+
+for i in range(len(Student_dt)):
+    for j in range(len(Student_dt)):
+        print(Student_dt[i][j], end=" ")
+    print()
+
+#Output 
+# list=[[1,11,111],[2,22,222,2222],[3,33,333,3333],[4,44,444,4444]]
+
+# for i in range(len(list)):
+#     for j in range(len(list[i])):
+#         print(list[i][j], end=" ")
+#     print()
